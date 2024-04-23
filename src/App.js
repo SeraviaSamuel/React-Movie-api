3b0bbb233317b6157ea1e8cd5a9e61795fca6875
SeraviaSamuel <seravia.sameh18@gmail.com>
2024-04-23 23:58:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+import React, { Suspense } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
+import MovieDetails from "./components/MovieDetails";
+import Watchlist from "./components/WatchList";
+import { LanguageProvider } from "./context/Language";
+// import RegisterForm from "./components/RegisterForm";
+const RegisterForm = React.lazy(()=>import('./components/RegisterForm'));
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <BrowserRouter>
+      <LanguageProvider>
+        <Header></Header>
+        <div className="container">
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Routes>
+              <Route path="/" element={<MovieList />} />
+              <Route path="movie-details/:id" element={<MovieDetails />} /> 
+              <Route path="watch-list" element={<Watchlist />} /> 
+              <Route path="form" element={<RegisterForm />} /> 
+              <Route path="*" element={<NotFound/>}/>
+            </Routes>
+          </Suspense>
+        </div>
+      </LanguageProvider>
+    </BrowserRouter>
+  
+    
   );
 }
 
